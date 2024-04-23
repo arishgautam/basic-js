e5ed4bc640aa06f4f4470c484e9952073d1f0e33
arishgautam <arishgautam686@gmail.com>
2024-04-23 20:48:35 +0545

    added events of js

---
+++ event.js
@@ -0,0 +1,33 @@
+// events in js
+let btn1 = document.querySelector("#btn1");
+// btn1.onclick = () => {
+// console.log("button was clicked ");
+// }
+// // with objcect
+// let btn2 = document.querySelector("#btn2");
+// btn2.onclick = (e) => {
+//     console.log(e);
+//     console.log(e.type);
+// }
+// event listners
+
+//add event listners
+
+btn1.addEventListener("click", () =>{
+   console.log("button was clicked handler1"); 
+})
+
+//handler 2 with object
+
+btn1.addEventListener("click", (e) =>{
+    console.log("button was clicked handler2");
+    console.log(e);
+ })
+ //handle3
+ const handler3 = () => {
+    console.log("button was clicked - handler3"); 
+ }
+ btn1.addEventListener("click", handler3);
+
+ // delete event listners
+ btn1.removeEventListener("click",handler3);
